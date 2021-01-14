b6312e3ba99ad90b084360cd61dda0eb35b10058
c247 <56177593+c247@users.noreply.github.com>
2021-01-14 18:01:45 -0500

    Update voting.c

--- voting.c
+++ voting.c
@@ -80,7 +80,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+   
     int mostvotes =-1;
     for (int i=0;i<candidate_count;++i)
     {
